148718b83f9423f131c38256207d47c2036bdb8d
Luis Enrique <le.gamboa@gmail.com>
2025-03-11 15:27:59 -0600

    commit 1b1 message Inicio 11/03/2025 #14

--- BugTracker/obj/Debug/net9.0/BugTracker.AssemblyInfo.cs
+++ BugTracker/obj/Debug/net9.0/BugTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b281b33a95e02ab74612562db19bbf43030499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ae45bea0f697564099c802ed564c449a8244f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
